a9917c855ebcfec0bd7648848ed8853d80a3b917
SyedSaifullahFaiaz <etheriousn16@gmail.com>
2024-05-15 21:38:31 +0100

    menu problem

--- problem solving/myapp.py
+++ problem solving/myapp.py
@@ -1,11 +1,21 @@
 num = input()
+
 num = num.split()
+X = int(num[0])
+Y = int(num[1])
 
-A = float(num[0])
-B = float(num[1])
-C = float(num[2])
-
-X1 = (-1 * B) + 0
+if X == 1:
+    print("Total: R$ ", f"{4.00 * Y:.2f}", end="\n")
+elif X == 2:
+    print("Total: R$ ", f"{4.50 * Y:.2f}", end="\n")
+elif X == 3:
+    print("Total: R$ ", f"{5.00 * Y:.2f}", end="\n")
+elif X == 4:
+    print("Total: R$ ", f"{2.00 * Y:.2f}", end="\n")
+elif X == 5:
+    print("Total: R$ ", f"{1.50 * Y:.2f}", end="\n")
+else:
+    num = input()
 
 
 
