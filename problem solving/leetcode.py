0f838a8cef4b6f7ee1aee68790439c6b36b3f759
SyedSaifullahFaiaz <etheriousn16@gmail.com>
2024-05-20 11:26:50 +0100

    leet code project file

---
+++ problem solving/leetcode.py
@@ -0,0 +1,7 @@
+num = list(input())
+
+sum = int(num[0]) + int(num[1])
+sum = int(sum) / int(sum)
+sum = int(sum) + int(sum)
+
+print(sum)
--- problem solving/myapp.py
+++ problem solving/myapp.py
@@ -4,18 +4,27 @@ num = num.split()
 X = int(num[0])
 Y = int(num[1])
 
-if X == 1:
-    print("Total: R$ ", f"{4.00 * Y:.2f}", end="\n")
-elif X == 2:
-    print("Total: R$ ", f"{4.50 * Y:.2f}", end="\n")
-elif X == 3:
-    print("Total: R$ ", f"{5.00 * Y:.2f}", end="\n")
-elif X == 4:
-    print("Total: R$ ", f"{2.00 * Y:.2f}", end="\n")
-elif X == 5:
-    print("Total: R$ ", f"{1.50 * Y:.2f}", end="\n")
-else:
-    num = input()
+Menu = {1 : 4.00, 2 : 4.50, 3 : 5.00, 4 : 2.00, 5 : 1.50}
+
+if X in Menu:
+    price = Menu[X]
+
+    total_cost = price * Y
+
+    print("Total: R$ ", f"{total_cost:.2f}", end="\n")
+
+# if X == 1:
+#     print("Total: R$ ", f"{4.00 * Y:.2f}", end="\n")
+# elif X == 2:
+#     print("Total: R$ ", f"{4.50 * Y:.2f}", end="\n")
+# elif X == 3:
+#     print("Total: R$ ", f"{5.00 * Y:.2f}", end="\n")
+# elif X == 4:
+#     print("Total: R$ ", f"{2.00 * Y:.2f}", end="\n")
+# elif X == 5:
+#     print("Total: R$ ", f"{1.50 * Y:.2f}", end="\n")
+# else:
+#     num = input()
 
 
 
